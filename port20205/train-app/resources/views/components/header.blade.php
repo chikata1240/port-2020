c7f705bc14e96392431f73ef8b3cae3a14b7b30a
Chikata yosuke <cy04kumakuma.mac.sec@gmail.com>
2020-06-09 17:40:55 +0900

    ヘッダーcss

--- port20205/train-app/resources/views/components/header.blade.php
+++ port20205/train-app/resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
 <div>
   <div class="header"></div>
   <div class="top-right links">
-    <a  href="">{{$nav_content1}}</a>
-    <a href="">{{$nav_content2}}</a>
+    <a href="/{{$navigation1}}">{{$nav_content1}}</a>
+    <a href="/{{$navigation2}}">{{$nav_content2}}</a>
   </div>
 </div>
